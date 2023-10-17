30940d3ff6b6d0889d9f4fbac394314a0da3fb27
Miljan Pandurevic <miljan.pandurevic123@gmail.com>
2023-10-17 23:22:18 +1100

    finished

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//multiply
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
 
-// Write your code here
+//random
+let random = Math.floor(Math.random() * 10) + 1;
+
+//mod
+let num3 = 19
+let num4 = 5
+
+let mod = num3 % num4
+
+//max
+let max = Math.max(2, 5, 10, 13, 20);
+
+
+
+
